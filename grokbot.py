58bfc53b3a735f46e3229ab02e5e772d861ed71e
FloatCobra <liamchelton@gmail.com>
2017-09-25 00:06:29 +0100

    Cleaned up and added basic db class

--- grokbot.py
+++ grokbot.py
@@ -32,11 +32,13 @@ import json
 import sys
 import os
 import re
+import sqlite3
+import textwrap
 
 
 class GrokBot(commands.Bot):
     '''
-    Custom Client for selfbot.py - Made by verix#7220
+    GrokBot!
     '''
     _mentions_transforms = {
         '@everyone': '@\u200beveryone',
@@ -69,26 +71,27 @@ class GrokBot(commands.Bot):
 
     def load_community_extensions(self):
         '''Loads up community extensions.'''
-        with open('data/community_cogs.txt') as fp:
+        with open('./data/community_cogs.txt') as fp:
             to_load = fp.read().splitlines()
         self.load_extensions(to_load, 'cogs.community')
 
     @property
     def token(self):
         '''Returns your token wherever it is'''
-        with open('data/config.json') as f:
+        with open('./data/config.json') as f:
             config = json.load(f)
             if config.get('TOKEN') == "your_token_here":
                 if not os.environ.get('TOKEN'):
                     self.run_wizard()
             else:
-                token = config.get('TOKEN').strip('\"')
+                #token = config.get('TOKEN').strip('\"')
+                token = "MzYxNDgyNjcxNDUwMzU3NzYy.DKkwmw.MZF4orBpH2xBZi1isx0s5MvvppU"
         return os.environ.get('TOKEN') or token
 
     @staticmethod
     async def get_pre(bot, message):
-        '''Returns the prefix.''' 
-        with open('data/config.json') as f: # TODO: server specific prefixes
+        '''Returns the prefix.'''
+        with open('./data/config.json') as f: # TODO: server specific prefixes
             prefix = json.load(f).get('PREFIX')
         return os.environ.get('PREFIX') or prefix or 'r.'
 
@@ -98,12 +101,12 @@ class GrokBot(commands.Bot):
         print('------------------------------------------')
         token = input('Enter your token:\n> ')
         print('------------------------------------------')
-        prefix = input('Enter a prefix for your selfbot:\n> ')
+        prefix = input('Enter a prefix for your bot:\n> ')
         data = {
                 "TOKEN" : token,
                 "PREFIX" : prefix,
             }
-        with open('data/config.json','w') as f:
+        with open('./data/config.json','w') as f:
             f.write(json.dumps(data, indent=4))
         print('------------------------------------------')
         print('Restarting...')
@@ -113,10 +116,10 @@ class GrokBot(commands.Bot):
     @classmethod
     def init(bot, token=None):
         '''Starts the actual bot'''
-        selfbot = bot()
-        safe_token = token or selfbot.token.strip('\"')
+        bot = bot()
+        safe_token = token or bot.token.strip('\"')
         try:
-            selfbot.run(safe_token, bot=False, reconnect=True)
+            bot.run(safe_token, reconnect=True)
         except Exception as e:
             print(e)
 
@@ -132,7 +135,7 @@ class GrokBot(commands.Bot):
         ---------------
         Client is ready!
         ---------------
-        Authors: verixx, fourjr, kwugfighter, add the rest here bois
+        Authors: verixx, fourjr, kwugfighter, FloatCobra, add the rest here bois
         ---------------
         Logged in as: {self.user}
         User ID: {self.user.id}
@@ -171,7 +174,26 @@ class GrokBot(commands.Bot):
             em_list = await embedtobox.etb(emb)
             for page in em_list:
                 await ctx.send(page)
+    class db:
+        '''Holds all the server info and bot info'''
+        def __init__(self):
+            self.path = "./data/config.db"
+            self.conn = sqlite3.connect(self.path)
+            self.cur = self.conn.cursor()
+            with self.conn:
+                struc = "(server_id INTEGER PRIMARY KEY UNIQUE, prefixes TEXT, modlog INTEGER, serverlog INTEGER,leave_enabled TEXT,autorole_enabled TEXT, log_enabled TEXT,join_enabled TEXT, join_message TEXT, leave_message TEXT, join_leave_channel INTEGER)"
+                self.conn.execute(f"CREATE TABLE IF NOT EXISTS config {struc}")
+                # I've stored all the bools as text because it's simple and it works
+        #note to self: use json.dumps(list) to store list of prefixes in db
+        #ps: use json.loads() to get the list back
 
-
+        def get_server(self, id):
+            """Returns a dict of all fields"""
+            self.conn.execute(f"SELECT * FROM config WHERE server_id = {id}")
+            row = self.cur.fetchone()
+            if row is None:
+                return None
+            else:
+                return {"server_id":row[0], "prefixes":row[1], "modlog":row[2], "serverlog":row[3], "leave_enabled":bool(row[4]), "autorole_enabled":bool(row[4]), "log_enabled":bool(row[5]), "join_enabled":bool(row[6]), "join_message":row[7], "leave_message":row[8], "join_leave_channel":row[9]}
 if __name__ == '__main__':
     GrokBot.init()
