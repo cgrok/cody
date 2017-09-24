bbd8b96ba1f3c0a1261e9616cb2d68015b9c998a
verixx <abdurraqeeb53@gmail.com>
2017-09-24 22:35:00 +1000

    Added the core of the bot

--- grokbot.py
+++ grokbot.py
@@ -18,14 +18,160 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+
 import discord
 from discord.ext import commands
+from ext.context import CustomContext
+from collections import defaultdict
+import asyncio
+import aiohttp
+import datetime
+import psutil
+import time
+import json
+import sys
+import os
+import re
 
 
 class GrokBot(commands.Bot):
-    def __init__(self):
-        super().__init__(commands_prefix=self.get_prefix)
+    '''
+    Custom Client for selfbot.py - Made by verix#7220
+    '''
+    _mentions_transforms = {
+        '@everyone': '@\u200beveryone',
+        '@here': '@\u200bhere'
+    }
+
+    _mention_pattern = re.compile('|'.join(_mentions_transforms.keys()))
+
+    def __init__(self, **attrs):
+        super().__init__(command_prefix=self.get_pre)
+        self.session = aiohttp.ClientSession(loop=self.loop)
+        self.process = psutil.Process()
+        self.extensions = [x.replace('.py', '') for x in os.listdir('cogs') if x.endswith('.py')]
+        self.messages_sent = 0
+        self.commands_used = defaultdict(int)
+        self.remove_command('help')
+        self.add_command(self.ping)
+        self.load_extensions()
+        self.load_community_extensions()
+
+    def load_extensions(self, cogs=None, path='cogs.'):
+        '''Loads the default set of extensions or a seperate one if given'''
+        for extension in cogs or self.extensions:
+            try:
+                self.load_extension(f'{path}{extension}')
+                print(f'Loaded extension: {extension}')
+            except Exception as e:
+                print(f'LoadError: {extension}\n'
+                      f'{type(e).__name__}: {e}')
+
+    def load_community_extensions(self):
+        '''Loads up community extensions.'''
+        with open('data/community_cogs.txt') as fp:
+            to_load = fp.read().splitlines()
+        self.load_extensions(to_load, 'cogs.community')
+
+    @property
+    def token(self):
+        '''Returns your token wherever it is'''
+        with open('data/config.json') as f:
+            config = json.load(f)
+            if config.get('TOKEN') == "your_token_here":
+                if not os.environ.get('TOKEN'):
+                    self.run_wizard()
+            else:
+                token = config.get('TOKEN').strip('\"')
+        return os.environ.get('TOKEN') or token
 
     @staticmethod
-    async def get_prefix(message):
-        pass
+    async def get_pre(bot, message):
+        '''Returns the prefix.''' 
+        with open('data/config.json') as f: # TODO: server specific prefixes
+            prefix = json.load(f).get('PREFIX')
+        return os.environ.get('PREFIX') or prefix or 'r.'
+
+    @staticmethod
+    def run_wizard():
+        '''Wizard for first start'''
+        print('------------------------------------------')
+        token = input('Enter your token:\n> ')
+        print('------------------------------------------')
+        prefix = input('Enter a prefix for your selfbot:\n> ')
+        data = {
+                "TOKEN" : token,
+                "PREFIX" : prefix,
+            }
+        with open('data/config.json','w') as f:
+            f.write(json.dumps(data, indent=4))
+        print('------------------------------------------')
+        print('Restarting...')
+        print('------------------------------------------')
+        os.execv(sys.executable, ['python'] + sys.argv)
+
+    @classmethod
+    def init(bot, token=None):
+        '''Starts the actual bot'''
+        selfbot = bot()
+        safe_token = token or selfbot.token.strip('\"')
+        try:
+            selfbot.run(safe_token, bot=False, reconnect=True)
+        except Exception as e:
+            print(e)
+
+    async def on_connect(self):
+        print('---------------\n'
+              'GrokBot connected!')
+
+    async def on_ready(self):
+        '''Bot startup, sets uptime.'''
+        if not hasattr(self, 'uptime'):
+            self.uptime = datetime.datetime.utcnow()
+        print(textwrap.dedent(f'''
+        ---------------
+        Client is ready!
+        ---------------
+        Authors: verixx, fourjr, add the rest here bois
+        ---------------
+        Logged in as: {self.user}
+        User ID: {self.user.id}
+        ---------------
+        '''))
+
+    async def on_command(self, ctx):
+        cmd = ctx.command.qualified_name.replace(' ', '_')
+        self.commands_used[cmd] += 1
+
+    async def process_commands(self, message):
+        '''Utilises the CustomContext subclass of discord.Context'''
+        ctx = await self.get_context(message, cls=CustomContext)
+        if ctx.command is None:
+            return
+        await self.invoke(ctx)
+
+    async def on_message(self, message):
+        '''Extra calculations'''
+        if message.author == self.user:
+            return
+        self.messages_sent += 1
+        self.last_message = time.time()
+        await self.process_commands(message)
+
+    @commands.command()
+    async def ping(self, ctx):
+        """Pong! Returns your websocket latency."""
+        em = discord.Embed()
+        em.title ='Pong! Websocket Latency:'
+        em.description = f'{self.ws.latency * 1000:.4f} ms'
+        em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
+        try:
+            await ctx.send(embed=em)
+        except discord.HTTPException:
+            em_list = await embedtobox.etb(emb)
+            for page in em_list:
+                await ctx.send(page)
+
+
+if __name__ == '__main__':
+    GrokBot.init()
