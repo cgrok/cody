c713549d43c1e3305b94c490f1a33d8531d0d6c0
FloatCobra <liamchelton@gmail.com>
2017-09-26 00:13:33 +0100

    added a bunch of stuff

--- ext/config.py
+++ ext/config.py
@@ -4,7 +4,7 @@ import discord
 
 class GuildConfig:
 
-    '''Class to aid with server specific configuration.'''
+    '''Class to aid with guild specific configuration.'''
 
     __slots__ = (
         'db', 'conn', 'cur', 'bot', 'id'
@@ -16,7 +16,8 @@ class GuildConfig:
         self.cur = database.cur
         self.bot = database.bot
         self.id = id
-
+        if self.db.get_value(self.id, "guild_id") is None:
+            self.db.set_default_config(self.id)
     @property
     def join_message(self):
         return self.db.get_value(self.id, 'join_message')
@@ -48,42 +49,68 @@ class GuildConfig:
 
     @property
     def modlog(self):
-        id = self.data.get('modlog')
+        id = self.db.get_value(self.id,'modlog')
         return self.bot.get_channel()
 
     @modlog.setter
     def modlog(self, channel):
         if isinstance(channel, int):
-            pass
+            id = channel
         elif isinstance(channel, discord.TextChannel):
             id = channel.id
-            pass
-
-    # TODO: Do the rest on the bottom
+        return self.db.set_value(self.id, 'modlog_channel', id)
 
     @property
     def guild(self):
         return self.bot.get_guild(self.id)
 
     @property
-    def name(self):
-        return self.guild.name
-
-    @property
     def welcome_channel(self):
         id = data.get('join_message_channel')
         return self.guild.get_channel(id)
+
+    @welcome_channel.setter
+    def set_welcome_channel(self, channel):
+        if isinstance(channel, int):
+            id = channel
+        elif isinstance(channel, discord.TextChannel):
+            id = channel.id
+        return self.db.set_value(self.id, 'welcome_channel', id)
 
     @property
     def leave_channel(self):
         id = data.get('leave_message_channel')
         return self.guild.get_channel(id)
 
-    def add_prefix(self):
-        pass # do stuff with db to add a prefix
+    @leave_channel.setter
+    def set_leave_channel(self, channel):
+        if isinstance(channel, int):
+            id = channel
+        elif isinstance(channel, discord.TextChannel):
+            id = channel.id
+        return self.db.set_value(self.id, 'leave_channel', id)
 
-    def remove_prefix(self):
-        pass # do stuff with db to remove prefix
+    @property
+    def prefixes(self):
+        prefixes = json.loads(self.db.get_value(self.id, 'prefixes'))
+        return prefixes
+
+    @prefixes.setter
+    def set_prefixes(self, prefixes):
+        prefixes = prefixes.split()
+        return self.db.set_value(self.id, 'prefixes', json.dumps(prefixes))
+
+    @property
+    def leave_enabled(self):
+        return bool(self.db.get_value(self.id, 'leave_enabled'))
+
+    #NOTE: This can either be a toggle switch or the current system
+    @leave_enabled.setter
+    def toggle_leave_enabled(self):
+        value = int(not self.db.get_value(self.id, 'leave_enabled')))
+        return self.db.set_value(self.id, 'leave_enabled', value)
+
+
 
 
 
@@ -91,7 +118,7 @@ class GuildConfig:
 class ConfigDatabase:
     '''Database functions'''
     def __init__(self, bot):
-        self.bot = bot
+
         self.path = "../data/config.db"
         self.conn = sqlite3.connect(self.path)
         self.cur = self.conn.cursor()
@@ -99,37 +126,33 @@ class ConfigDatabase:
 
     def create_config_table(self):
         self.conn.execute("""
-            CREATE TABLE IF NOT EXISTS config 
-            (server_id INTEGER PRIMARY KEY UNIQUE, 
-            prefixes TEXT, 
-            modlog_channel INTEGER,
-            join_channel INTEGER, 
-            leave_channel INTEGER,
-            leave_enabled TEXT,
-            autorole_enabled TEXT, 
-            modlog_enabled TEXT,
-            join_enabled TEXT, 
-            join_message TEXT, 
-            leave_message TEXT, 
-            selfroles TEXT)""")
+            CREATE TABLE IF NOT EXISTS config
+            (guild_id INTEGER PRIMARY KEY UNIQUE,
+            prefixes TEXT,
+            modlog_channel INT,
+            join_channel INT,
+            leave_channel INT,
+            leave_enabled INT(1),
+            autorole_enabled INT(1),
+            modlog_enabled INT(1),
+            join_enabled INT(1),
+            join_message TEXT,
+            leave_message TEXT,
+            selfroles TEXT,
+            autorole INT)""")
 
-    def set_default_config(self, server_id):
+    def set_default_config(self, guild_id):
         with self.conn:
-            self.cur.execute("""
-                INSERT INTO config VALUES(
-                ?,?, # serverid and prefixes
-                0, 0, 0, 0, 0, 0, 0, # Channel IDs and bools 
-                "Welcome to {server.name}, {user.mention}!", # join message
-                "Bye Bye {user.name}!", # leave message
-                "[]" # selfroles
-                )""",(server_id,"[\"g.\"]"))
+            list = (guild_id,"[\"g.\"]",0,0,0,0,0,0,0,"Welcome {user.mention} to {guild.name}","Bye Bye {user.name}!","[]",0)
+            self.cur.execute(f"INSERT INTO config VALUES {list}")
 
-    def get_server(self, server_id):
+    def get_guild(self, guild_id):
         """Returns a dict of all fields"""
-        return GuildConfig(self, server_id)
+        return GuildConfig(self, guild_id)
 
-    def get_data(self, server_id):
-        self.cur.execute("SELECT * FROM config WHERE server_id = ?",(server_id,))
+    def get_data(self, guild_id):
+        """Returns a raw dict of all fields"""
+        self.cur.execute("SELECT * FROM config WHERE guild_id = ?",(guild_id,))
         columns = [x[0] for x in self.cur.description]
         rows = self.cur.fetchone()
         if rows is None:
@@ -140,15 +163,10 @@ class ConfigDatabase:
             raw_dict['selfroles'] = json.loads(raw_dict['selfroles'])
             return raw_dict
 
-    def get_value(self, server_id, column):
-        self.cur.execute("SELECT ? FROM config WHERE server_id = ?", (column, server_id))
+    def get_value(self, guild_id, column):
+        self.cur.execute(f"SELECT {column} FROM config WHERE guild_id = {guild_id}")
         return self.cur.fetchone()
 
-    def set_value(self, server_id, column, new_val):
+    def set_value(self, guild_id, column, new_val):
         with self.conn:
-            self.cur.execute("UPDATE config SET ? = ? WHERE server_id = ?", (column, new_val, server_id))
-
-
-
-
-
+            self.cur.execute(f"UPDATE config SET {column} = {new_val} WHERE guild_id = {guild_id}")
