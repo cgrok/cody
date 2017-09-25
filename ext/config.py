309a45dba5e0dbfadb19c335c17338af382694f3
verixx <abdurraqeeb53@gmail.com>
2017-09-25 15:42:45 +1000

    Improved the guildconfig class
    I made it so that its dynamic, you can set things that will directly affect the db.

--- ext/config.py
+++ ext/config.py
@@ -1,4 +1,6 @@
-
+import sqlite3
+import json
+import discord
 
 class GuildConfig:
 
@@ -21,37 +23,135 @@ class GuildConfig:
     '''
 
     __slots__ = (
-        'announce_channel', 'autorole', 'autorole_enabled', 'leave_enabled', 
-        'leave_message', 'log_channel', 'log_enabled', 'name', 'prefixes', 
-        'selfroles','id', 'join_enabled', 'join_message','join_message_channel',
-        'leave_message_channel'
+        'autorole', 'autorole_enabled', 'leave_enabled', 
+        'leave_message', 'modlog', 'log_enabled', 'name', 'prefixes', 
+        'selfroles','id', 'join_enabled', 'join_message','db', 'bot'
         )
 
-    def __init__(self, **data):
-        self.id = data.get('id')
-        self.join_enabled = False
-        self.leave_enabled = False
-        self.autorole_enabled = False
-        self.log_enabled = False
-        self.from_data(data)
-
+    def __init__(self, database, **data):
+        self.db = database
+        self.bot = db.bot
+        self.data = data
+        self.id = data.get('server_id')
+        
     def from_data(self, data):
-        self.name = data.get('name')
-        self.prefixes = data.get('prefixes', ['g.'])
-        self.join_message = data.get('join_message')
-        self.join_message_channel = data.get('join_message_channel')
-        self.leave_message = data.get('join_message')
-        self.leave_message_channel = data.get('leave_message_channel')
-        self.autorole = data.get('autorole')
+        self._join_enabled = data.get('join_enabled')
+        self._leave_enabled = data.get('leave_enabled')
+        self._autorole_enabled = data.get('autorole_enabled')
+        self._log_enabled = data.get('log_enabled')
+        self.prefixes = json.loads(data.get('prefixes', "['g.']"))
         self.selfroles = data.get('selfroles')
-        self.announce_channel = data.get('announce_channel')
-        self.log_channel = data.get('log_channel')
 
-    def add_prefix(self, prefix):
-        self.prefixes += [prefix]
+    @property
+    def join_message(self):
+        return self.data.get('join_message')
+        # this needs to be chnaged to be dynamic
+        # something like: self.db.get_join_message()
 
-    def remove_prefix(self, prefix):
-        try:
-            self.prefixes.remove(prefix)
-        except:
-            return False
+    @join_message.setter
+    def join_message(self, msg):
+        pass # do stuff to update the join message
+
+    @property
+    def leave_message(self):
+        return data.get('leave_message')
+
+    @leave_message.setter
+    def leave_message(self, msg):
+        pass # do stuff to update leave message
+
+    @property
+    def autorole(self):
+        id = self.data.get('autorole')
+        return discord.utils.get(self.guild.roles, id=id)
+
+    @autorole.setter
+    def autorole(self, role):
+        if isinstance(role, int):
+            pass # do stuff
+        else:
+            role = role.id
+            pass # do stuff
+
+    @property
+    def modlog(self):
+        id = self.data.get('modlog')
+        return self.bot.get_channel()
+
+    @modlog.setter
+    def modlog(self, channel):
+        if isinstance(channel, int):
+            pass
+        elif isinstance(channel, discord.TextChannel):
+            id = channel.id
+            pass
+
+    # TODO: restructure the db
+
+
+    @property
+    def guild(self):
+        return self.bot.get_guild(self.id)
+
+    @property
+    def name(self):
+        return self.guild.name
+
+    @property
+    def welcome_channel(self):
+        id = data.get('join_message_channel')
+        return self.guild.get_channel(id)
+
+    @property
+    def leave_channel(self):
+        id = data.get('leave_message_channel')
+        return self.guild.get_channel(id)
+
+    def add_prefix(self):
+        pass # do stuff with db to add a prefix
+
+    def remove_prefix(self):
+        pass # do stuff with db to remove prefix
+
+
+
+
+
+class DataBase:
+    '''Database functions'''
+    def __init__(self, bot):
+        self.bot = bot
+        self.path = "../data/config.db"
+        self.conn = sqlite3.connect(self.path)
+        self.cur = self.conn.cursor()
+        self.create_db()
+
+
+    def create_db(self):
+        with self.conn:
+            struc = """(server_id INTEGER PRIMARY KEY UNIQUE, 
+                        prefixes TEXT, 
+                        modlog INTEGER, 
+                        leave_enabled TEXT,
+                        autorole_enabled TEXT, 
+                        log_enabled TEXT,
+                        join_enabled TEXT, 
+                        join_message TEXT, 
+                        leave_message TEXT, 
+                        join_leave_channel INTEGER)"""
+
+            cur = self.conn.execute(f"CREATE TABLE IF NOT EXISTS config {struc}")
+
+
+    def get_server(self, id):
+        """Returns a dict of all fields"""
+        self.cur.execute(f"SELECT * FROM config WHERE server_id = {id}")
+        columns = [x[0] for x in self.cur.description]
+        rows = self.cur.fetchone()
+        if rows is None:
+            return None
+        else:
+            raw_dict = {k:v if '_enabled' not in k else bool(v) for k, v in zip(columns, rows)}
+            return GuildConfig(self, **raw_dict)
+
+
