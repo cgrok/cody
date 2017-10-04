c6a071a914251a37590ab2801305e146ac0c7236
「XΛOS」 <30846531+XAOS1502@users.noreply.github.com>
2017-10-04 20:07:00 +0530

    Added settle command
    This one has more options than the normal ones out there

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,10 +1,36 @@
 import discord
+import random
 from discord.ext import commands
 from datetime import date
+from enum import Enum
 from urllib.request import urlopen
 
 halloween = date(2017, 10, 31)
 christmas = date(2017, 12, 25)
+
+class RPSLS(Enum):
+    rock     = "\N{RAISED FIST}"
+    paper    = "\N{RAISED HAND WITH FINGERS SPLAYED}"
+    scissors = "\N{BLACK SCISSORS}"
+    lizard   = "\N{LIZARD}"
+    spock    = "\N{RAISED HAND WITH PART BETWEEN MIDDLE AND RING FINGERS}"
+
+
+class RPSLSParser:
+    def __init__(self, argument):
+        argument = argument.lower()
+        if argument == "rock":
+            self.choice = RPSLS.rock
+        elif argument == "paper":
+            self.choice = RPSLS.paper
+        elif argument == "scissors":
+            self.choice = RPSLS.scissors
+        elif argument == "lizard":
+            self.choice = RPSLS.lizard
+        elif argument == "spock":
+            self.choice = RPSLS.spock
+        else:
+            raise
 
 
 class Misc:
@@ -103,6 +129,58 @@ class Misc:
             await ctx.channel.send(tinyurl)
         else:
             pass
+
+    @commands.command(aliases=['rock', 'paper', 'scissors', 'lizard', 'spock', 'rps'])
+    async def settle(self, ctx, your_choice : RPSLSParser = None):
+        """Play rock paper scissors lizard spock"""
+        if your_choice != None:
+            author = ctx.message.author.display_name
+            grok = self.bot.user.name
+            player_choice = your_choice.choice
+            available = RPSLS.rock, RPSLS.paper, RPSLS.scissors, RPSLS.lizard, RPSLS.spock
+            bot_choice = random.choice((available))
+            cond = {
+                    (RPSLS.rock,     RPSLS.paper)    : False,
+                    (RPSLS.rock,     RPSLS.scissors) : True,
+                    (RPSLS.rock,     RPSLS.lizard)   : True,
+                    (RPSLS.rock,     RPSLS.spock)    : False,
+                    (RPSLS.paper,    RPSLS.rock)     : True,
+                    (RPSLS.paper,    RPSLS.scissors) : False,
+                    (RPSLS.paper,    RPSLS.lizard)   : False,
+                    (RPSLS.paper,    RPSLS.spock)    : True,
+                    (RPSLS.scissors, RPSLS.rock)     : False,
+                    (RPSLS.scissors, RPSLS.paper)    : True,
+                    (RPSLS.scissors, RPSLS.lizard)   : True,
+                    (RPSLS.scissors, RPSLS.spock)    : False,
+                    (RPSLS.lizard,   RPSLS.rock)     : False,
+                    (RPSLS.lizard,   RPSLS.paper)    : True,
+                    (RPSLS.lizard,   RPSLS.scissors) : False,
+                    (RPSLS.lizard,   RPSLS.spock)    : True,
+                    (RPSLS.spock,    RPSLS.rock)     : True,
+                    (RPSLS.spock,    RPSLS.paper)    : False,
+                    (RPSLS.spock,    RPSLS.scissors) : True,
+                    (RPSLS.spock,    RPSLS.lizard)   : False
+                   }
+            em = discord.Embed()
+            em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
+            em.add_field(name=f'{author}', value=f'{player_choice.value}', inline=True)
+            em.add_field(name=f'{grok}', value=f'{bot_choice.value}', inline=True)
+            if bot_choice == player_choice:
+                outcome = None
+            else:
+                outcome = cond[(player_choice, bot_choice)]
+            if outcome is True:
+                em.set_footer(text="You win!")
+                await ctx.channel.send(embed=em)
+            elif outcome is False:
+                em.set_footer(text="You lose...")
+                await ctx.channel.send(embed=em)
+            else:
+                em.set_footer(text="We're square")
+                await ctx.channel.send(embed=em)
+        else:
+            msg = 'rock, paper, scissors, lizard, OR spock'
+            await ctx.channel.send(f'Enter: `{ctx.prefix}{ctx.invoked_with} {msg}`', delete_after=5)
 
 
 def setup(bot):
