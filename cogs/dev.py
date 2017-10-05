dead7191ac5cbaf181fa71a4aae7174ad2865493
FloatCobra <liamchelton@gmail.com>
2017-10-06 00:06:13 +0100

    fixed an error

--- cogs/dev.py
+++ cogs/dev.py
@@ -68,7 +68,7 @@ class Developer:
     @commands.command()
     async def cmd_help(self, ctx):
         """Test the help function"""
-        await bot.send_cmd_help(ctx)
+        await self.bot.send_cmd_help(ctx)
 
     @commands.command(aliases=["reload"])
     async def reloadcog(self, ctx, *, cog: str):
