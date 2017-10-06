f22d6a23840e1ba59b02fca55f9662782bc1f20a
FloatCobra <liamchelton@gmail.com>
2017-10-06 20:35:07 +0100

    another commit, more to come, stay tuned!!!

--- cogs/member.py
+++ cogs/member.py
@@ -19,13 +19,13 @@ class Member:
 
     @commands.group(no_pm=True)
     async def memberset(self, ctx):
-        
+
         #if ctx.invoked_subcommand is None:
             #await self.bot.send_cmd_help(ctx)
 
         em = discord.Embed(color=discord.Colour.red(), description="Member Settings")
-        em.add_field(name="Join Message Enabled", value=bool(ctx.config.join_enabled), inline=False)
-        #em.add_field(name="Leave Message Enabled", value=bool(ctx.config.leave_enabled), inline=False)
+        em.add_field(name="Join Message Enabled", value=str(bool(ctx.config.join_enabled)), inline=False)
+        #em.add_field(name="Leave Message Enabled", value=str(bool(ctx.config.leave_enabled)), inline=False)
         #em.add_field(name="Welcome Channel", value=self.bot.get_channel(ctx.config.welcome_channel.id).mention, inline=False)
         #em.add_field(name="Leave Channel", value=self.bot.get_channel(ctx.config.leave_channel.id).mention, inline=False)
         #em.add_field(name="Join Message", value=ctx.config.join_message, inline=False)
